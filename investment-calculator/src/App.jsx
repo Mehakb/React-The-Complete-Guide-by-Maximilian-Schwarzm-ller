2b2103dfa71001c9d9d633e6db920463e822e5b7
Mehak <Mehak1921@yahoo.com>
2024-02-27 00:50:26 +0530

    Add results component

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Results from './components/Results';
 import UserInput from './components/UserInput';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: +newValue,
+    }));
+  };
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
--- investment-calculator/src/components/UserInput.jsx
+++ investment-calculator/src/components/UserInput.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-
-export default function UserInput() {
+export default function UserInput({ userInput, onChange }) {
   return (
     <section id='user-input'>
       <div className='input-group'>
         <p>
           <label>Initial Investment</label>
-          <input type='number' required />
+          <input
+            type='number'
+            onChange={(e) => onChange('initialInvestment', e.target.value)}
+            value={userInput.initialInvestment}
+            required
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type='number' required />
+          <input
+            type='number'
+            onChange={(e) => onChange('annualInvestment', e.target.value)}
+            value={userInput.annualInvestment}
+            required
+          />
         </p>
       </div>
       <div className='input-group'>
         <p>
           <label>Expected Return</label>
-          <input type='number' required />
+          <input
+            type='number'
+            onChange={(e) => onChange('expectedReturn', e.target.value)}
+            value={userInput.expectedReturn}
+            required
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type='number' required />
+          <input
+            type='number'
+            onChange={(e) => onChange('duration', e.target.value)}
+            value={userInput.duration}
+            required
+          />
         </p>
       </div>
     </section>
