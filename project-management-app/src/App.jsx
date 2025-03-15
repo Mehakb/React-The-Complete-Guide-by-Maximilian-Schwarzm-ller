31eaed237df73f27ed89ef604dc745b179ce984b
Mehak bansal <mehak1921@yahoo.com>
2025-03-16 02:33:07 +0530

    feat: add validations and handle cancel button

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -11,28 +11,49 @@ function App() {
 
   const handleStartAddProject = () => {
     setProjectsState((prevState) => {
-      return { ...prevState, selectedProjectId: null };
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  const handleCancelAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
     });
   };
 
   const handleAddProject = (projectData) => {
     setProjectsState((prevState) => {
       const newProject = { ...projectData, id: Math.random() };
-      return { ...prevState, projects: [...prevState.projects, newProject] };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
     });
   };
 
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
